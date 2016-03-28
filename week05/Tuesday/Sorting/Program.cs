bf2a7c7724935b4974daf13b320ef6214c696b3c
Antonio <amindov@abv.bg>
2016-03-29 02:44:06 +0300

    Sorting

---
+++ week05/Tuesday/Sorting/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+
+	class MyIntComparer : IComparer<int>
+	{
+		public int Compare(int x, int y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+
+	class LastDigitComparer : IComparer<int>
+	{
+		public int Compare(int x, int y)
+		{
+			int xLast = x % 10;
+			int yLast = y % 10;
+
+			return xLast.CompareTo(yLast);
+		}
+	}
+
+	class StringLengthComparer : IComparer<string>
+	{
+		public int Compare(string x, string y)
+		{
+			return x.Length.CompareTo(y.Length);
+		}
+	}
+
+	class OddEvenComparer : IComparer<int?>
+	{
+		public int Compare(int? x, int? y)
+		{
+			if (x == y) return 0;
+
+			if (x == null)
+			{
+				return -1;
+			}
+
+			if (y == null)
+			{
+				return 1;
+			}
+
+			bool xEven = x.Value%2 == 0;
+			bool yEven = y.Value%2 == 0;
+
+			if (xEven == yEven)
+			{
+				if (xEven)
+				{
+					return x.Value.CompareTo(y.Value)*(-1);
+				}
+
+				return x.Value.CompareTo(y.Value);
+			}
+
+			if (xEven) return -1;
+
+			return 1;
+		}
+	}
+	
+	class ReverseComparer<T> : IComparer<T>
+		where T : IComparable
+	{
+		public int Compare(T x, T y)
+		{
+			return x.CompareTo(y) * (-1);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var array = new[] { 2, 4, 1, 6, 10 };
+			var sortedArray = (int[])array.SelectionSort();
+
+			foreach (var i in sortedArray)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
---
+++ week05/Tuesday/Sorting/SortAndSearchExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Sorting
+{
+	public static class SortAndSearchExtensions
+	{
+		public static IList<T> BubbleSort<T>(this IList<T> list, IComparer<T> comparer)
+		{
+			for (var i = 0; i < list.Count - 2; i++)
+			{
+				for (var j = i + 1; j < list.Count - 1; j++)
+				{
+					if (comparer.Compare(list[i], list[j]) > 0)
+					{
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+				}
+			}
+
+			return list;
+		}
+
+		public static IList<T> BubbleSort<T>(this IList<T> list)
+		{
+			return BubbleSort(list, Comparer<T>.Default);
+		}
+
+		public static IList<T> SelectionSort<T>(this IList<T> list, IComparer<T> comparer)
+		{
+			var minIndex = 0;
+
+			for(var i = 0; i < list.Count - 2; i++)
+			{
+				for(var j = i + 1; j < list.Count - 1; j++)
+				{
+					if(comparer.Compare(list[minIndex], list[j]) > 0)
+					{
+						minIndex = j;
+					}
+				}
+
+				if(i == minIndex) continue;
+
+				var temp = list[i];
+				list[i] = list[minIndex];
+				list[minIndex] = temp;
+			}
+
+			return list;
+		}
+
+		public static IList<T> SelectionSort<T>(this IList<T> list)
+		{
+			return SelectionSort(list, Comparer<T>.Default);
+		}
+
+
+	}
+}
