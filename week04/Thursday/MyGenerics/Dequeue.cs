e681dc7a75b48433c0ba0af27126640c52e706de
Antonio <amindov@abv.bg>
2016-03-29 03:04:29 +0300

    Dequeue

--- week04/Thursday/MyGenerics/Dequeue.cs
+++ week04/Thursday/MyGenerics/Dequeue.cs
@@ -1,4 +1,7 @@
-﻿namespace Stack
+﻿using System;
+using System.Data;
+
+namespace Stack
 {
     public class Dequeue<T>
     {
@@ -11,6 +14,7 @@
         {
             data = new T[capacity];
             this.capacity = capacity;
+	        size = 0;
 
             front = back = capacity / 2;
         }
@@ -19,11 +23,97 @@
         {
             this.data = data;
             capacity = data.Length;
+	        size = capacity;
+	        front = 0;
+	        back = data.Length - 1;
         }
 
         public void Clear()
         {
             front = back = capacity / 2;
         }
-    }
+
+		public bool Contains(T item)
+	    {
+			for (var i = front; i < back; i++)
+			{
+				if (item.Equals(data[i]))
+				{
+					return true;
+				}
+			}
+
+			return false;
+	    }
+
+	    public T RemoveFromFront()
+	    {
+		    if (size <= 0)
+		    {
+			    throw new InvalidOperationException("Dequeue empty");
+		    }
+
+		    front ++;
+		    size --;
+		    return data[front - 1];
+	    }
+
+	    public T RemoveFromEnd()
+	    {
+			if(size <= 0)
+			{
+				throw new InvalidOperationException("Dequeue empty");
+			}
+
+			back--;
+			size--;
+			return data[back + 1];
+		}
+
+	    public void AddToFront(T item)
+	    {
+		    if (front == 0)
+		    {
+			    Resize();
+		    }
+
+		    data[--front] = item;
+		    size ++;
+	    }
+
+	    public void AddToEnd(T item)
+	    {
+		    if (back == capacity - 1)
+		    {
+			    Resize();
+		    }
+
+		    data[++back] = item;
+		    size++;
+	    }
+
+	    public T PeekFromFront()
+	    {
+		    return data[front];
+	    }
+
+	    public T PeekFromEnd()
+	    {
+		    return data[back];
+	    }
+
+	    private void Resize()
+	    {
+		    var oldData = data;
+
+		    capacity *= 2;
+			data = new T[capacity];
+		    int startIndex = capacity/2 - size/2;
+
+		    for (int i = 0; i < size; i++)
+		    {
+			    data[startIndex + i] = oldData[front + i];
+		    }
+	    }
+	}
 }
